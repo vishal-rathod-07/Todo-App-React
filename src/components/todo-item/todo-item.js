720e5d6c781d61f8830e0fad2f47e79e6959f51c
Vishal Rathod <rvd31305@gmail.com>
2022-03-04 17:37:12 +0530

    Todo item component and styles added
    - Todo item component and styles added

---
+++ src/components/todo-item/todo-item.js
@@ -0,0 +1,32 @@
+import "./todo-item.css";
+
+const TodoItem = ({ todo, completeTodo, removeTodo }) => {
+  return (
+    <li className='todo-item' key={todo.id}>
+      <input
+        id={todo.id}
+        type='checkbox'
+        className='todo-item__checkbox'
+        title={todo.isCompleted ? "Mark as incomplete" : "Mark as complete"}
+        checked={todo.isCompleted}
+        onChange={() => completeTodo(todo.id)}
+      />
+      <label
+        htmlFor={todo.id}
+        className='todo-item__label'
+        title={todo.isCompleted ? "Mark as incomplete" : "Mark as complete"}
+      >
+        {todo.text}
+      </label>
+      <button
+        className='todo-item__button'
+        title='Remove Task From List'
+        onClick={() => removeTodo(todo.id)}
+      >
+        &#x2715;
+      </button>
+    </li>
+  );
+};
+
+export default TodoItem;
